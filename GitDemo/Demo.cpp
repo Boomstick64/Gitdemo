905055deca440a6841dd533d0707bb2273a92ff6
Thomas Tidy <thomastidy@gmail.com>
2022-05-11 16:18:30 +0930

    Added a Demo project to solution

---
+++ GitDemo/Demo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to the Australian Science program-aided enrichment center." << endl;
+    cout << "We hope your time in the classroom has been a pleasant one." << endl;
+    cout << "For your own safety and the safety of others, please ______." << endl;
+}
