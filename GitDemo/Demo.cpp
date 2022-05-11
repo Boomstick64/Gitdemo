36e86e6c5a021fde604e5b4098b3676206789bf1
Thomas Tidy <thomastidy@gmail.com>
2022-05-11 16:36:33 +0930

    added gibberish

--- GitDemo/Demo.cpp
+++ GitDemo/Demo.cpp
@@ -6,5 +6,5 @@ int main()
 {
     cout << "Welcome to the Australian Science program-aided enrichment center." << endl;
     cout << "We hope your time in the classroom has been a pleasant one." << endl;
-    cout << "For your own safety and the safety of others, please ______." << endl;
+    cout << "For your own safety and the safety of others, please bibno crelpre" << endl;
 }
